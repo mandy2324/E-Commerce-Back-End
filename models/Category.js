59a4ead0d355af84ca84407a7f4e5c9361be537a
unknown <jasdeepsaini30@gmail.com>
2021-11-19 15:38:00 -0500

    adding models

--- models/Category.js
+++ models/Category.js
@@ -4,17 +4,25 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
+Category.init({
     // define columns
-  },
-  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    // define a category_name column
+    category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
-);
+});
 
 module.exports = Category;
